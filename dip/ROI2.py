560844d46c4bf03ee7efd4750b19eaa358c80c00
Axmdstar <ahaji222@gmail.com>
2025-04-26 20:41:37 +0300

    ...

--- dip/ROI2.py
+++ dip/ROI2.py
@@ -5,8 +5,8 @@ img1 = cv2.imread("images3.png")
 img2 = cv2.imread("images2.png")
 
 # Display original shape
-cv2.imshow("Src1", img1)
-cv2.imshow("Src2", img2)
+# cv2.imshow("Src1", img1)
+# cv2.imshow("Src2", img2)
 
 # Define a ROIs:
 roi1 = img1[246:953, 120:961]
@@ -14,12 +14,14 @@ roi2 = img2[197:814, 18:747]
 roi2 = cv2.resize(roi2, (roi1.shape[1], roi1.shape[0]))
 
 # Show the ROI
-cv2.imshow("ROI1", roi1)
-cv2.imshow("ROI2", roi2)
+# cv2.imshow("ROI1", roi1)
+# cv2.imshow("ROI2", roi2)
 
 # Modify the region
 img1[246:953, 120:961] = roi2
 
 # Show the modified image
-cv2.imshow("Dest", img1)
-cv2.waitKey(0)
+while True:
+    cv2.imshow("Dest", img1)
+    if cv2.waitKey(1) == ord("x"):
+        break
--- dip/camera.py
+++ dip/camera.py
@@ -1,16 +1,15 @@
 import cv2 as cv
 
+# choose video capture method, 0 is for default camera
 cam = cv.VideoCapture(0)
 
 if not cam.isOpened():
     print("Error to Open Camera")
     exit()
 
-print("Cam is Open")
-
-
-fourcc = cv.VideoWriter_fourcc(*'XVID')  # Codec
-out = cv.VideoWriter('./CamVideo/output.avi', fourcc, 20.0, (640, 480))
+# video save format and output path to write to
+fourcc = cv.VideoWriter_fourcc(*"XVID")  # Codec
+out = cv.VideoWriter("./CamVideo/output.avi", fourcc, 20.0, (640, 480))
 
 
 print("Recording started. Press 'q' to stop...")
@@ -18,19 +17,19 @@ print("Recording started. Press 'q' to stop...")
 while True:
     # Capture frame-by-frame
     ret, frame = cam.read()
-    
+
     if not ret:
         print("Error: Can't receive frame. Exiting...")
         break
-    
+
     # Write the frame to file
     out.write(frame)
-    
+
     # Display the frame (optional)
-    cv.imshow('Webcam Feed', frame)
-    
+    cv.imshow("Webcam Feed", frame)
+
     # Exit on 'q' key press
-    if cv.waitKey(1) == ord('q'):
+    if cv.waitKey(1) == ord("q"):
         break
 
 # Release everything when done
